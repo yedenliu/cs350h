1bfd60f85b1967f7951443b4381925d5ee3f657d
Eden Liu <edenliu100@gmail.com>
2023-03-18 16:21:05 -0400

    first commit of new file

---
+++ prepared_queries.py
@@ -0,0 +1,86 @@
+import cs304dbi as dbi
+
+def get_abbrev(conn):
+    curs = dbi.cursor(conn)
+    sql =   ''' select distinct dept 
+                from courses 
+                where dept <> ""
+                order by dept asc 
+            '''
+    curs.execute(sql)
+    deptsTups = curs.fetchall()
+    deptsList = [i[0] for i in deptsTups]
+    return deptsList
+
+def check_course_exists(conn, dept, cnum):
+    curs = dbi.cursor(conn)
+    sql = '''   select cid from courses
+                where dept = %s and cnum = %s
+            '''
+    curs.execute(sql, [dept, cnum])
+    return len(curs.fetchall()) > 0
+
+
+def get_dept_courses(conn, abbrev):
+    '''
+    Finds courses given an department abbreviation
+    
+    Param - connection object, department abbreviation 
+    Return - list of courses (in form CS 111)
+    '''
+    curs = dbi.cursor(conn)
+    
+    sql =   ''' select dept, cnum from courses 
+                where dept = %s
+            '''
+    curs.execute(sql, [abbrev])
+    return curs.fetchall()
+
+def get_dept_courses(conn, abbrev):
+    '''
+    Finds courses given an department abbreviation
+    
+    Param - connection object, department abbreviation 
+    Return - list of courses
+    '''
+    curs = dbi.cursor(conn)
+    
+    sql =   ''' select dept, cnum from courses 
+                where dept = %s and cnum =%s
+            '''
+    curs.execute(sql, [abbrev])
+    return curs.fetchall()
+
+def get_two_level(conn, abbrev):
+    '''
+    Finds courses given an department abbreviation at the 200-level
+    
+    Param - connection object, department abbreviation 
+    Return - list of courses
+    '''
+    curs = dbi.cursor(conn)
+    level = '2%'
+    sql =   ''' select dept, cnum
+                from courses 
+                where dept = %s
+                and cnum like %s
+            '''
+    curs.execute(sql, [abbrev, level])
+    return curs.fetchall()
+
+def get_three_level(conn, abbrev):
+    '''
+    Finds courses given an department abbreviation at the 300-level
+    
+    Param - connection object, department abbreviation 
+    Return - list of courses
+    '''
+    curs = dbi.cursor(conn)
+    level = '3%'
+    sql =   ''' select dept, cnum
+                from courses 
+                where dept = %s
+                and cnum like %s
+            '''
+    curs.execute(sql, [abbrev, level])
+    return curs.fetchall()
